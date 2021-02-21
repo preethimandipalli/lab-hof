72c64a953ffc9f5d7cb38aa2c04ff5e20c326f9c
Vinod Kumar Reddy <vinodkumarreddy@MacBook-Air-4.local>
2021-02-21 17:51:14 +0530

    first-commit

---
+++ hof/src/App.js
@@ -0,0 +1,13 @@
+import HigherOrderFunctions from '/Users/vinodkumarreddy/Desktop/hof/src/HigherOrderComponent.jsx';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <HigherOrderFunctions/>
+ 
+    </div>
+  );
+}
+
+export default App;
